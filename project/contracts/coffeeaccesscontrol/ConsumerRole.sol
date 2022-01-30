021133de4f15d4c373f95e71a9f288eef843bd07
barbokupna <barbokupna@gmail.com>
2022-01-30 17:13:56 -0600

    Project-Tests+SmartContracts

--- project/contracts/coffeeaccesscontrol/ConsumerRole.sol
+++ project/contracts/coffeeaccesscontrol/ConsumerRole.sol
@@ -8,45 +8,47 @@ contract ConsumerRole {
   using Roles for Roles.Role;
 
   // Define 2 events, one for Adding, and other for Removing
-  event AddConsumer(address account);
-  event RemoveConsumer(address account);
+  event ConsumerAdd(address account);
+  event ConsumerRemove(address account);
 
   // Define a struct '' by inheriting from 'Roles' library, struct Role
   Roles.Role private consumers;
   
   // In the constructor make the addconsumersress that deploys this contract the 1st consumer
   constructor() public {
-    
+    _addConsumer(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyConsumer() {
-    
+    require(consumers.has(msg.sender));
     _;
   }
 
   // Define a function 'isConsumer' to check this role
   function isConsumer(address account) public view returns (bool) {
-    
+    return consumers.has(account);
   }
 
   // Define a function 'addConsumer' that adds this role
   function addConsumer(address account) public onlyConsumer {
-    
+    _addConsumer(account);
   }
 
   // Define a function 'renounceConsumer' to renounce this role
   function renounceConsumer() public {
-    
+    _removeConsumer(msg.sender);
   }
 
   // Define an internal function '_addConsumer' to add this role, called by 'addConsumer'
   function _addConsumer(address account) internal {
-    
+    consumers.add(account);
+    emit ConsumerAdd(account);
   }
 
   // Define an internal function '_removeConsumer' to remove this role, called by 'removeConsumer'
   function _removeConsumer(address account) internal {
-    
+    consumers.remove(account);
+    emit ConsumerRemove(account);
   }
 }
--- project/truffle.js
+++ project/truffle.js
@@ -1,9 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "c72941ff3c594156aa6ffda1433e851e";
+//
+const fs = require('fs');
+const mnemonic = "motor satoshi bean taxi client lyrics process artist noodle punch person scissors";
+
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4 ,  // rinkeby's id
+     
+        from: "0x3de5326e4FA15F48761DF56e1911AEf09a8cab14"
     }
   }
 };
